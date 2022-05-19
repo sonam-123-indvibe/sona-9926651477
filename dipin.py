5c00b923eb4bcbae3d74fcebf5106bbc2e4d5b98
sonam-123-indvibe <88430627+sonam-123-indvibe@users.noreply.github.com>
2022-05-19 10:50:46 +0530

    Add files via upload

---
+++ dipin.py
@@ -0,0 +1,55 @@
+'''#relational/comparision:
+#conditional/control stat...:
+1.if_else
+a.simple if
+b.if_else
+c.if_elif_else
+d.nested if_else
+2.loops
+a.while loop
+b.for in loop
+
+a.simple if
+syntex:
+    if(be/condition):
+        true stat...
+a=int(input("enter the no"))
+if(a>0):
+    print("a is grtere")
+
+
+b.if_else
+syntex:
+    if(be/condition):
+        true stat...
+    else:
+        false stat..
+      
+a=int(input("enter the no"))
+if(a>0):
+    print("a is grtere")
+else:
+    print("a is less than zero")
+'''
+##wap to check user is eligible for vote or not??
+ #wap to check no. is +ve or -ve??
+ #wap to check no. is even or not??
+ #compare a no with 17 if no. is gretre than 17 then return absolute diff
+ if not return square of diff??
+ #comapre a 3 no. if all are equal then return
+ sum of all if not than return sum and thrice of all??
+ #comapre a two no. and find gretest b/w them?/
+ #take values  of length and breath of a rectangle from user and check if it is square or not?/
+ #a company decided to give a bonus of 5% to
+employee if the year of service is more than 5  years ask user for their salary and year of service and print the net bonus??
+ #a student will not be allowed to sit in exam her
+attendance is less than 75% take following input from user number of classes held
+,number of clasees attend is student to sit in exam or not??
+
+
+
+
+
+
+
+
